e5e07d6edce4e170d19ab6ee02197ab1e06e5777
mkfsn <p408865@gmail.com>
2016-07-06 20:21:45 +0900

    Modularized but only support one hotel: 徳山駅新幹線口

--- ToyokoInn.py
+++ ToyokoInn.py
@@ -8,136 +8,154 @@ from re import sub
 import requests
 from pyquery import PyQuery
 from datetime import datetime
+from urllib import quote
 
 
 class ToyokoInn(object):
 
     def __init__(self, name):
+        import json
         self.name = name
+
+        with open("hotel.json") as f:
+            data = filter(lambda x: x['name'].endswith(name), json.load(f))[:1]
+
+        if data:
+            self.dataid = data[0]['dataid']
+            self.state = data[0]['state']
+            self.substateid = data[0]['substateid']
+        print data
+
+    def _extract_price_remain(self, html, index):
+        selector_s = "tbody > tr:eq(%d) > td:gt(0)" % index
+
+        result = []
+        for td in PyQuery(html).children(selector_s):
+            item = PyQuery(td).find("table > tbody div tr:eq(1)")
+            
+            price = PyQuery(item).find("td > span").text()
+            price = int(sub(r'[^\d.]', '', price))
+
+            remain = PyQuery(item).find("td + td + td").text()
+            remain = 0 if remain == u'\xd7' or remain == '' else int(remain)
+
+            result.append([price, remain])
+        return result
+
+    def _extract(self, html):
+        pq = PyQuery(html).find(
+            "form div.BlockFormClndr table.BlockSrchClndr3"
+        )
+
+        selector_ = "tr:eq(11) th"
+        date_order = [PyQuery(v).text() for v in PyQuery(pq).find(selector_)]
+        self.data = {d: {} for d in date_order}
+
+        index = 9
+        while index < 1000:
+            # Get type of room
+            selector_ = "tbody > tr:eq(%d) th:eq(0)" % index
+            room = PyQuery(pq).children(selector_).text()
+            if room == '':
+                break
+
+            for date, v in self.data.items():
+                self.data[date][room] = {'member': [], 'guest': []}
+
+            for i, v in enumerate(self._extract_price_remain(pq, index)):
+                self.data[date_order[i]][room]['member'] = v
+
+            for i, v in enumerate(self._extract_price_remain(pq, index + 2)):
+                self.data[date_order[i]][room]['guest'] = v
+
+            index += 3
+
+        return self.data
+
+    def room(self, date=None, member=False):
+        s = requests.session()
+
+        baseurl = 'https://yoyaku.4and5.com/reserve/html/rvpc_srchHtl.html'
+        param = {
+            'tlDtl': 'true',
+            'cntry': 'JPN',
+            'chcknYearAndMnth': '%d%02d' % (date['year'], date['month']),
+            'chcknDayOfMnth': '%02d' % (date['day']),
+            'ldgngNum': '1',
+            'roomNum': '1',
+            'roomClssId': '',
+            'fvrtName': '',
+            'prfctr': self.state,
+            'htlName': quote(self.name.encode("UTF-8")),
+            'language': 'ja',
+            'dispFull': 'on',
+            'ldgngPpl': '1',
+            'id': self.dataid,
+            'ref': 'info',
+        }
+        url = baseurl + '?' + '&'.join([k + '=' + v for k, v in param.items()])
+
+        headers = {
+            'User-Agent': ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) ' +
+                           'AppleWebKit/537.36 (KHTML, like Gecko) ' + 
+                           'Chrome/51.0.2704.103 Safari/537.36'),
+            'Accept': ('text/html,application/xhtml+xml,application/xml;q=0.9' +
+                       ',image/webp,*/*;q=0.8'),
+            'Accept-Encoding': 'gzip, deflate, sdch, br',
+            'Accept-Language': 'zh-TW,zh;q=0.8,en-US;q=0.6,en;q=0.4,ja;q=0.2',
+        }
+
+        # session GET
+        r = s.get(url, headers=headers)
+
+        url = 'https://yoyaku.4and5.com/reserve/html/rvpc_srchHtl.html'
+        data = {
+            'rg.seasar.ymir.token': '89dbd02e37b4597e8862423dc09a4c0d',
+            'language': '',
+            'htlUid': '',
+            'srchRoomAllFlag': 'true',
+            'srchRoomBasicFlag': 'false',
+            'srchRoomPlanFlag': 'false',
+            'tabGrpCode': 'ALL',
+            'langFlag': 'ja',
+            'dsplLinkPnt': '',
+            'hospital': '',
+            'cntry': 'JPN',
+            'prfctr': self.state,
+            'mncplty': '',
+            'clsstSttn': '',
+            'htlName': self.name,
+            'chcknYearAndMnth': '%d%02d' % (date['year'], date['month']),
+            'chcknDayOfMnth': '%02d' % date['day'],
+            'ldgngNum': '1',
+            'ldgngPpl': '2',
+            'roomNum': '2',
+            'dispFull': 'on',
+        }
+
+        # session POST
+        r = s.post(url, headers=headers, data=data, allow_redirects=False)
+
+        baseurl = 'https://yoyaku.4and5.com/reserve/html/rvpc_srchHtl.html'
+        param = 'clndr[6000136][][6000136][][20071001]'
+        url = baseurl + '?' + param
+        r = s.post(url, headers=headers, data=data)
+
+        result = []
+        data = self._extract(r.text.encode('utf-8'))
+        for room, item in data['%s/%s' % (int(date['month']), int(date['day']))].items():
+            price, remain = item['member'] if member else item['guest']
+            if remain == 0:
+                continue
+            result.append('%s %s %s' % (room, price, remain))
+        return result
 
 
 def fetch(year, month, day, member=False):
-    # Results of dates. {DATE: {ROOM: {member: PRICE, guest: PRICE}}}
-    results = {}
-
-    s = requests.session()
-
-    baseurl = 'https://yoyaku.4and5.com/reserve/html/rvpc_srchHtl.html'
-    param = {
-        'tlDtl': 'true',
-        'cntry': 'JPN',
-        'chcknYearAndMnth': '{year}{month}'.format(year=year, month=month),
-        'chcknDayOfMnth': '{day}'.format(day=day),
-        'ldgngNum': '1',
-        'roomNum': '1',
-        'roomClssId': '',
-        'fvrtName': '',
-        'prfctr': '35',
-        'htlName': '%E5%BE%B3%E5%B1%B1%E9%A7%85%E6%96%B0%E5%B9%B9%E7%B7%9A%E5%8F%A3',
-        'language': 'ja',
-        'dispFull': 'on',
-        'ldgngPpl': '1',
-        'id': '131',
-        'ref': 'info',
-        '_ga': '1.151929791.720274849.1467717947',
-    }
-    url = baseurl + '?' + '&'.join([k + '=' + v for k, v in param.items()])
-
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        'Accept-Encoding': 'gzip, deflate, sdch, br',
-        'Accept-Language': 'zh-TW,zh;q=0.8,en-US;q=0.6,en;q=0.4,ja;q=0.2',
-    }
-
-    # session GET
-    r = s.get(url, headers=headers)
-
-    url = 'https://yoyaku.4and5.com/reserve/html/rvpc_srchHtl.html'
-    data = {
-        'rg.seasar.ymir.token': '89dbd02e37b4597e8862423dc09a4c0d',
-        'language': '',
-        'htlUid': '',
-        'srchRoomAllFlag': 'true',
-        'srchRoomBasicFlag': 'false',
-        'srchRoomPlanFlag': 'false',
-        'tabGrpCode': 'ALL',
-        'langFlag': 'ja',
-        'dsplLinkPnt': '',
-        'hospital': '',
-        'cntry': 'JPN',
-        'prfctr': '35',
-        'mncplty': '',
-        'clsstSttn': '',
-        'htlName': '徳山駅新幹線口',
-        'chcknYearAndMnth': '%s%s' % (year, month),
-        'chcknDayOfMnth': '%s' % day,
-        'ldgngNum': '1',
-        'ldgngPpl': '2',
-        'roomNum': '2',
-        'dispFull': 'on',
-    }
-
-    # session POST
-    r = s.post(url, headers=headers, data=data, allow_redirects=False)
-
-    baseurl = 'https://yoyaku.4and5.com/reserve/html/rvpc_srchHtl.html'
-    param = 'clndr[6000136][][6000136][][20071001]'
-    url = baseurl + '?' + param
-    r = s.post(url, headers=headers, data=data)
-
-    html = r.text.encode('utf-8')
-    pq = PyQuery(html).find("form div.BlockFormClndr table.BlockSrchClndr3")
-
-    date_order = [PyQuery(v).text() for v in PyQuery(pq).find("tr:eq(11) th")]
-    results = {d: {} for d in date_order}
-
-    index = 9
-    while index < 100:
-        # Get type of room
-        room = PyQuery(pq).children("tbody > tr:eq(%d) th:eq(0)" % index).text()
-        if room == '':
-            break
-
-        for date, v in results.items():
-            results[date][room] = {'member': [], 'guest': []}
-
-        i_ = 0
-        for td in PyQuery(pq).children("tbody > tr:eq(%d) > td:gt(0)" % index):
-            item = PyQuery(td).find("table > tbody div tr:eq(1)")
-            price = sub(r'[^\d.]', '', PyQuery(item).find("td > span").text()[1:])
-            remain = PyQuery(item).find("td + td + td").text()
-            remain = 0 if remain == u'\xd7' or remain == '' else int(remain)
-
-            results[date_order[i_]][room]['member'] = [price, remain]
-            i_ += 1
-
-        i_ = 0
-        for td in PyQuery(pq).children("tbody > tr:eq(%d) > td:gt(0)" % (index + 2)):
-            item = PyQuery(td).find("table > tbody div tr:eq(1)")
-            price = sub(r'[^\d.]', '', PyQuery(item).find("td > span").text()[1:])
-            remain = PyQuery(item).find("td + td + td").text()
-            remain = 0 if remain == u'\xd7' or remain == '' else int(remain)
-
-            results[date_order[i_]][room]['guest'] = [price, remain]
-            i_ += 1
-        index += 3
-
-    # List all
-    # for date, _item in results.items():
-    #     for room, item in results[date].items():
-    #         price, remain = item['member'] if member else item['guest']
-    #         if remain == 0:
-    #             continue
-    #         print date, room, price, remain
-
-    retval = []
-    for room, item in results['%s/%s' % (int(month), int(day))].items():
-        price, remain = item['member'] if member else item['guest']
-        if remain == 0:
-            continue
-        retval.append('%s %s %s' % (room, price, remain))
-    return retval
+    hotel = ToyokoInn(u"徳山駅新幹線口")
+    date = {'year': int(year), 'month': int(month), 'day': int(day)}
+    data = hotel.room(date=date, member=member)
+    return data
 
 
 if __name__ == '__main__':
