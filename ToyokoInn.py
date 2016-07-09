b057a55cc2ca386e13b65b91f4a33da6067ac1c0
mkfsn <p408865@gmail.com>
2016-07-09 12:40:09 +0900

    Move application to example

--- ToyokoInn.py
+++ ToyokoInn.py
@@ -158,60 +158,9 @@ class ToyokoInn(object):
         return result
 
 
-def fetch(year, month, day, member=False):
+if __name__ == '__main__':
+    year, month, day, member = [2016, 7, 22, True]
     hotel = ToyokoInn(u"徳山駅新幹線口")
     date = {'year': int(year), 'month': int(month), 'day': int(day)}
-    data = hotel.room(date=date, member=member)
-    return data
-
-
-if __name__ == '__main__':
-    import argparse
-    import os
-    import sys
-    import json
-
-    parser = argparse.ArgumentParser(
-        description='Fetch ToyokoInn reservable room'
-    )
-    parser.add_argument('--year', type=int,
-                        required=True, help='Specify year of date')
-    parser.add_argument('--month', type=int, choices=xrange(1, 13),
-                        required=True, help='Specify month of date')
-    parser.add_argument('--day', type=int, choices=xrange(1, 32),
-                        required=True, help='Specify day of date')
-
-    args = parser.parse_args()
-    if datetime.now() > datetime(args.year, args.month, args.day, 23, 59, 59):
-        sys.exit(-1)
-
-    year, month, day = [args.year, '%02d' % args.month, '%02d' % args.day]
-    result = fetch(year, month, day, member=True)
-
-    title = "徳山駅新幹線口-%s-%s-%s" % (year, month, day)
-
-    changed = False
-
-    filename = '/tmp/%s' % title
-    if not os.path.isfile(filename):
-        changed = True
-    else:
-        with open(filename, 'r+') as f:
-            previous = f.read()
-            if previous != json.dumps(result):
-                changed = True
-
-    with open(filename, 'w') as f:
-        f.write(json.dumps(result))
-
-    if not changed:
-        sys.exit(0)
-
-    content = '[%s]\n%s' % (title.decode('utf-8'), '\n'.join(result))
-    data = {
-        'username': 'ToyokoINN-bot',
-        'text': content,
-        'channel': 'toyokoinn',
-    }
-    slack_url = 'https://hooks.slack.com/services/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'
-    requests.post(slack_url, data=json.dumps(data))
+    rooms = hotel.room(date=date, member=member)
+    print rooms
